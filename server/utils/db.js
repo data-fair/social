23da16ee69ccd5a7f2114f72557a7a4d6f2a7271
Alban Mouton <alban.mouton@gmail.com>
2021-11-25 16:55:40 +0100

    chore: unnecessary mongodb stuff in latest version

--- server/utils/db.js
+++ server/utils/db.js
@@ -17,17 +17,8 @@ exports.ensureIndex = async (db, collection, key, options = {}) => {
 }
 
 exports.connect = async () => {
-  let client
-  const opts = { useNewUrlParser: true, useUnifiedTopology: true }
   debug('Connecting to mongodb ' + config.mongoUrl)
-  try {
-    client = await MongoClient.connect(config.mongoUrl, opts)
-  } catch (err) {
-    // 1 retry after 1s
-    // solve the quite common case in docker-compose of the service starting at the same time as the db
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    client = await MongoClient.connect(config.mongoUrl, opts)
-  }
+  const client = await MongoClient.connect(config.mongoUrl)
   const db = client.db()
   return { db, client }
 }
