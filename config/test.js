81554d750fd7beee307c585f8ffcdb155762b2df
kernoeb <kernoeb@protonmail.com>
2024-04-25 17:15:07 +0200

    Update test.js

--- config/test.js
+++ config/test.js
@@ -2,5 +2,5 @@ module.exports = {
   port: 4658,
   publicUrl: 'http://localhost:4658',
   wsPublicUrl: 'ws://localhost:4658',
-  directoryUrl: 'http://localhost:8080'
+  directoryUrl: 'http://localhost:6001'
 }
